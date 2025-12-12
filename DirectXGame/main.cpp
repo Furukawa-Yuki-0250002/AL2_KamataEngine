c127293aad2a1fcd76a6e922a757881e6210b7c4
Furukawa-Yuki-0250002 <k025g0002@g.neec.ac.jp>
2025-12-12 15:46:19 +0900

    mainの訂正
    GitHubリポジトリ作成時に追加するファイルを間違えたので訂正

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,9 +1,39 @@
 #include <Windows.h>
+#include "KamataEngine.h"
+#include "GameScene.h"
+
+// グローバル変数宣言
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	
-	// ブランチ作成テスト
+	KamataEngine::Initialize(L"LC1A_28_フルカワ_ユウキ_タイトル");
 
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// ゲームシーンのインスタンス生成
+	GameScene* gameScene = new GameScene();
+
+	// ゲームシーンの初期化
+	gameScene->Initialize();
+
+	while (true) {
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		gameScene->Update();
+
+		dxCommon->PreDraw();
+
+		gameScene->Draw();
+
+		dxCommon->PostDraw();
+	}
+
+	KamataEngine::Finalize();
+	delete gameScene;
+	gameScene = nullptr;
 	return 0;
 }
